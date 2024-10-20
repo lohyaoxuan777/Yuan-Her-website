951ba9c77bee4e7176ea443da12da7a6c4a22690
lohyaoxuan777 <jaydenloh57@gmail.com>
2024-10-20 12:19:35 +0800

    deleted:    data/database.db 	modified:   rockpaperscissors.css 	modified:   rockpaperscissors.css.map 	modified:   rockpaperscissors.html 	modified:   sass/rockpaperscissors.scss 	modified:   savethepeopleonearth.html 	modified:   searchagame.html 	modified:   src/rockpaperscissors.js

--- src/rockpaperscissors.js
+++ src/rockpaperscissors.js
@@ -1,50 +1,39 @@
-let rockBtn = document.querySelector(".rockbtn");
-let paperBtn = document.querySelector(".paperbtn");
-let scissorsBtn = document.querySelector(".scissorsbtn");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
 
-let botChoice = document.querySelector(".botchoose");
-let userChoice = document.querySelector(".userchoose");
-let Choises = ["Rock", "Paper", "Scissors"];
-let gameInfo = document.querySelector(".gameinfo");
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.querySelector(".playerDisplay");
+const computerDisplay = document.querySelector(".computerDisplay");
+const resultDisplay = document.querySelector(".resultDisplay");
 
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
 
-rockBtn.addEventListener("click", () => {
-    botChoice.innerHTML = "Scissors";
-    userChoice.innerHTML = "Rock";
-});
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ?  "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ?  "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ?  "YOU WIN!" : "YOU LOSE!";
+                break;
 
-paperBtn.addEventListener("click", () => {
-    botChoice.innerHTML = "Paper";
-    userChoice.innerHTML = "Paper";
-});
+        }
+    }
 
-scissorsBtn.addEventListener("click", () => {
-    botChoice.innerHTML = "Rock";
-    userChoice.innerHTML = "Scissors";
-});
-
-
-if (botChoice.innerHTML == "Rock" && userChoice.innerHTML == "Paper") {
-    gameInfo.innerHTML = "You win";
-    alert("You win");
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
 };
 
-if (botChoice.innerHTML == "Rock" && userChoice.innerHTML == "Scissors") {
-    gameInfo.innerHTML = "You lose";
-};
-
-if (botChoice.innerHTML == "Paper" && userChoice.innerHTML == "Rock") {
-    gameInfo.innerHTML = "You lose";
-};
-
-if (botChoice.innerHTML == "Paper" && userChoice.innerHTML == "Scissors") {
-    gameInfo.innerHTML = "You win";
-};
-
-if (botChoice.innerHTML == "Scissors" && userChoice.innerHTML == "Rock") {
-    gameInfo.innerHTML = "You win";
-};
-
-if (botChoice.innerHTML == "Scissors" && userChoice.innerHTML == "Paper") {
-    gameInfo.innerHTML = "You lose";
-};
+rock.addEventListener("click", () => playGame("rock"));
+paper.addEventListener("click", () => playGame("paper"));
+scissors.addEventListener("click", () => playGame("scissors"))
