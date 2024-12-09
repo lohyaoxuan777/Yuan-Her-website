51d084558b7e5e3f0957ee96f7f3c1bea2bb33ce
lohyaoxuan777 <jaydenloh57@gmail.com>
2024-12-09 21:22:25 +0800

    modified:   css/style.css 	modified:   css/typethiswords.css 	modified:   package.json 	modified:   searchagame.html 	modified:   src/typethiswords.js 	modified:   typethiswords.html 	new file:   typethiswords.php

--- src/typethiswords.js
+++ src/typethiswords.js
@@ -1,3 +1,17 @@
-let gameCanvas = document.querySelector(`canvas`);
+"use strict";
+let gameCanvas = document.querySelector('canvas');
 let canvasContext = gameCanvas.getContext('2d');
+let playgameBtn = document.querySelector('.playbtn');
 
+function waitSleep(ms) {
+    return new Promise(reslove, () => {
+        setTimeout(reslove, ms);
+    });
+};
+
+function startGame() {
+    console.log("Started Game, please get ready!");
+    
+};
+
+playgameBtn.addEventListener('click', startGame);
